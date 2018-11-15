a51ba6a3ad0f0c53c79f54920e58a81e70487412
Felix Becker <felix.b@outlook.com>
2018-11-15 14:35:25 +0100

    Track request duration

--- server/src/dispatcher.ts
+++ server/src/dispatcher.ts
@@ -1,6 +1,7 @@
 import LightstepSpan from 'lightstep-tracer/lib/imp/span_imp'
 import { Span, Tracer } from 'opentracing'
 import { ERROR } from 'opentracing/lib/ext/tags'
+import * as prometheus from 'prom-client'
 import { Observable, Subject } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 import { CancellationToken, CancellationTokenSource } from 'vscode-jsonrpc'
@@ -48,6 +49,13 @@ export function createDispatcher(
     const handlers = new Map<string, RequestHandler<any, any>>()
     const notifications = new Subject<NotificationMessage>()
 
+    const requestDurationMetric = new prometheus.Histogram({
+        name: 'jsonrpc_request_duration_seconds',
+        help: 'The JSON RPC request latencies in seconds',
+        labelNames: ['success', 'method'],
+        buckets: [0.1, 0.2, 0.5, 0.8, 1, 1.5, 2, 5, 10, 15, 20, 30],
+    })
+
     client.reader.listen(async message => {
         if (isNotificationMessage(message)) {
             if (message.method === '$/cancelRequest') {
@@ -60,64 +68,74 @@ export function createDispatcher(
                 notifications.next(message)
             }
         } else if (isRequestMessage(message)) {
-            const handler = handlers.get(message.method)
-            if (handler) {
-                const span = tracer.startSpan('Handle ' + message.method)
-                let lightstepTraceUrl: string | undefined
-                // Log the trace URL for this request in the client
-                if (span instanceof LightstepSpan) {
-                    lightstepTraceUrl = span.generateTraceURL()
-                    logger.log(`Trace ${message.method} ${lightstepTraceUrl}`)
+            const stopTimer = requestDurationMetric.startTimer()
+            let success: boolean
+            const span = tracer.startSpan('Handle ' + message.method)
+            let lightstepTraceUrl: string | undefined
+            // Log the trace URL for this request in the client
+            if (span instanceof LightstepSpan) {
+                lightstepTraceUrl = span.generateTraceURL()
+                logger.log(`Trace ${message.method} ${lightstepTraceUrl}`)
+            }
+            span.setTag('method', message.method)
+            if (isRequestMessage(message)) {
+                span.setTag('id', message.id)
+            }
+            const cancellationTokenSource = new CancellationTokenSource()
+            cancellationTokenSources.set(message.id, cancellationTokenSource)
+            const token = cancellationTokenSource.token
+            let response: ResponseMessage | undefined
+            try {
+                const handler = handlers.get(message.method)
+                if (!handler) {
+                    throw Object.assign(new Error('No handler for method ' + message.method), {
+                        code: ErrorCodes.MethodNotFound,
+                    })
                 }
-                span.setTag('method', message.method)
+                const result = await Promise.resolve(handler(message.params, token, span))
+                success = true
+                response = {
+                    jsonrpc: '2.0',
+                    id: message.id,
+                    result,
+                }
+            } catch (err) {
+                span.setTag(ERROR, true)
+                success = false
+                logErrorEvent(span, err)
+
+                if (!isAbortError(err)) {
+                    logger.error('Error handling message\n', message, '\n', err)
+                }
                 if (isRequestMessage(message)) {
-                    span.setTag('id', message.id)
-                }
-                const cancellationTokenSource = new CancellationTokenSource()
-                cancellationTokenSources.set(message.id, cancellationTokenSource)
-                const token = cancellationTokenSource.token
-                try {
-                    const result = await Promise.resolve(handler(message.params, token, span))
-                    const response: ResponseMessage = {
+                    const code = isAbortError(err)
+                        ? ErrorCodes.RequestCancelled
+                        : typeof err.code === 'number'
+                        ? err.code
+                        : ErrorCodes.UnknownErrorCode
+                    response = {
                         jsonrpc: '2.0',
                         id: message.id,
-                        result,
-                    }
-                    if (lightstepTraceUrl) {
-                        ;(response as any)._trace = lightstepTraceUrl
-                    }
-                    client.writer.write(response)
-                } catch (err) {
-                    span.setTag(ERROR, true)
-                    logErrorEvent(span, err)
-
-                    if (!isAbortError(err)) {
-                        logger.error('Error handling message\n', message, '\n', err)
-                    }
-                    if (isRequestMessage(message)) {
-                        const code = isAbortError(err)
-                            ? ErrorCodes.RequestCancelled
-                            : typeof err.code === 'number'
-                            ? err.code
-                            : ErrorCodes.UnknownErrorCode
-                        const errResponse = {
-                            jsonrpc: '2.0',
-                            id: message.id,
-                            error: {
-                                message: err.message,
-                                code,
-                                data: {
-                                    stack: err.stack,
-                                    ...err,
-                                },
+                        error: {
+                            message: err.message,
+                            code,
+                            data: {
+                                stack: err.stack,
+                                ...err,
                             },
-                        }
-                        client.writer.write(errResponse)
+                        },
                     }
-                } finally {
-                    cancellationTokenSources.delete(message.id)
-                    span.finish()
                 }
+            } finally {
+                cancellationTokenSources.delete(message.id)
+                span.finish()
+            }
+            if (response) {
+                if (lightstepTraceUrl) {
+                    ;(response as any)._trace = lightstepTraceUrl
+                }
+                client.writer.write(response)
+                stopTimer({ success: success + '', method: message.method })
             }
         }
     })
