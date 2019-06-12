77f9ac712b3802c5acdcefb16338d648abba4917
Felix Becker <felix.b@outlook.com>
2019-06-12 23:29:05 +0200

    Filter for all code actions

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -18,7 +18,7 @@ import {
 import delayPromise from 'delay'
 import { merge } from 'ix/asynciterable/index'
 import { filter, map, scan, tap } from 'ix/asynciterable/pipe/index'
-import { fromPairs, uniqBy, uniqWith, isEqual } from 'lodash'
+import { fromPairs, isEqual, uniqWith } from 'lodash'
 import { Span, Tracer } from 'opentracing'
 import * as path from 'path'
 import { BehaviorSubject, from, fromEventPattern, Subscription } from 'rxjs'
@@ -276,7 +276,7 @@ export async function activate(ctx: sourcegraph.ExtensionContext): Promise<void>
                     try {
                         const codeActions = await connection.sendRequest(CodeActionRequest.type, codeActionParams)
                         codeActionDecorations.push(
-                            ...uniqWith(asArray(codeActions), isEqual).map(
+                            ...asArray(codeActions).map(
                                 (codeAction): sourcegraph.TextDocumentDecoration => ({
                                     range: convertRange(diagnostic.range),
                                     after: {
@@ -293,7 +293,10 @@ export async function activate(ctx: sourcegraph.ExtensionContext): Promise<void>
                 for (const appWindow of sourcegraph.app.windows) {
                     for (const viewComponent of appWindow.visibleViewComponents) {
                         if (viewComponent.document.uri === sourcegraphTextDocumentUri.href) {
-                            viewComponent.setDecorations(codeActionsDecorationType, codeActionDecorations)
+                            viewComponent.setDecorations(
+                                codeActionsDecorationType,
+                                uniqWith(codeActionDecorations, isEqual)
+                            )
                             viewComponent.setDecorations(
                                 diagnosticsDecorationType,
                                 params.diagnostics.map(convertDiagnosticToDecoration)
